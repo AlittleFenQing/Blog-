eb530b8a3db92568197b5bad468a4c72785e0e7f
wangxue <wangxue@mioji.com>
2020-01-06 17:26:03 +0800

    List+author

--- blog/components/Header.js
+++ blog/components/Header.js
@@ -5,11 +5,11 @@ import { Row, Col, Menu, Icon } from 'antd';
 const Header = () => (
   <div className="header">
     <Row type='flex' justify='center'>
-      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+      <Col xs={24} sm={24} md={10} lg={15} xl={12}>
         <span className='header-logo'>王雪</span>
         <span className='header-txt'>前端开发经验积累</span>
       </Col>
-      <Col xs={0} sm={0} md={14} lg={14} xl={14}>
+      <Col xs={0} sm={0} md={14} lg={8} xl={6}>
         <Menu mode='horizontal'>
           <Menu.Item>
             <Icon type='home' theme="twoTone" twoToneColor="#eb2f96" />
