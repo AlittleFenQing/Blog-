007dd28bd21a631e5a5dec036309046e9373874b
wangxue <wangxue@mioji.com>
2020-01-06 16:43:10 +0800

    Header components

---
+++ blog/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../static/style/components/header.css';
+import { Row, Col, Menu, Icon } from 'antd';
+
+const Header = () => (
+  <div className="header">
+    <Row type='flex' justify='center'>
+      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+        <span className='header-logo'>王雪</span>
+        <span className='header-txt'>前端开发经验积累</span>
+      </Col>
+      <Col xs={0} sm={0} md={14} lg={14} xl={14}>
+        <Menu mode='horizontal'>
+          <Menu.Item>
+            <Icon type='home' theme="twoTone" twoToneColor="#eb2f96" />
+            首页
+          </Menu.Item>
+          <Menu.Item>
+            <Icon type="heart" theme="twoTone" twoToneColor="#eb2f96" />
+            知识梳理
+          </Menu.Item>
+          <Menu.Item>
+            <Icon type='smile' theme="twoTone" twoToneColor="#eb2f96" />
+            闲言碎语
+          </Menu.Item>
+        </Menu>
+      </Col>
+    </Row>
+  </div>
+)
+
+export default Header
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
 import { Button } from 'antd';
+import Header from '../components/Header'
 
 const Home = () => (
   <div>
     <Head>
       <title>home</title>
     </Head>
-    <Button type="primary">我是按钮</Button>
+    <Header />
   </div>
 )
 
