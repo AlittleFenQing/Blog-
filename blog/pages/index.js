b75c434ebdca4b0e9e9a7e29760129be63866025
wangxue <wangxue@mioji.com>
2020-01-06 15:09:37 +0800

    blog init

---
+++ blog/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
+import { Button } from 'antd';
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>home</title>
+    </Head>
+    <Button type="primary">我是按钮</Button>
+  </div>
+)
+
+export default Home;
